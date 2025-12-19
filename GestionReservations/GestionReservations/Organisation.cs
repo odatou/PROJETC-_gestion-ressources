f0b15d3cfffdc09431c97c88b60bc7f902a089b9
odatou <odalsontoussaint1@gmail.com>
2025-12-19 09:45:28 -0500

    Ajout de la classe abstraite Organisation

---
+++ GestionReservations/GestionReservations/Organisation.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Organisation{
+
+    public string Nom{ get ; set;}
+
+    public string Adresse{get; set;}
+
+    public string Email{get; set;} 
+
+    public abstract void AfficherRecapitulatif();
+
+}
